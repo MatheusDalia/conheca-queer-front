fd9552cf79e48b50457f41f628079340c328a7d7
matheusdalia <matheusdalia8@gmail.com>
2022-06-13 02:42:14 -0300

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,24 @@
+import { Banner } from './banner';
+import { Card } from "./cards";
+import Carousel from './carousel';
+import { History } from './history';
+import { Intro } from "./intro";
+import { Footer } from './footer';
+import { News } from './News';
+import { Navbar } from './navbar';
+import { Personality } from './personality';
+import { Projects } from './projects';
+
+
+export {
+    Banner,
+    Card,
+    Carousel,
+    History,
+    Intro,
+    News,
+    Projects,
+    Personality,
+    Footer,
+    Navbar,
+}
